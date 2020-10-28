7a6aee111858917dc789242bcc7096569ad11fd1
Leonardo <fanger53@gmail.com>
2020-10-28 14:39:17 -0500

    add check_winner test

--- spec/TicTacToe_spec.rb
+++ spec/TicTacToe_spec.rb
@@ -1,22 +1,25 @@
-
-require '../lib/game_logic'
+require './lib/game_logic'
+# require './bin/main'
 
 describe Game do
     let(:game) {Game.new}
-   
+    let(:arr) {%w[x x x ]}
+    let(:simbol){'o'}
     # describe "#display_board" do
     #     it "should display the initial board of the game" do 
     #     expect(game.).to eq(@board)
     #     end
     # end
 
-    describe "#check_move" do
-
-        it "should replace the move of the player with the simbol" do
-        move = 1
-        simbol = 'X'
-        game.check_move(move, simbol)
+    describe '#check_move' do
+        it 'should replace the move of the player with the simbol' do
         expect(game.check_move(0, 'X')).to eq(true)
         end
+    end
+
+    describe '#check_winner'do
+      it 'should return false if the combination of simbols are not right' do
+        expect(game.check_winner(simbol)).to eql(false)
+      end
     end
 end
