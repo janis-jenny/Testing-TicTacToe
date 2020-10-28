f1e6fbd09e2be998aef934cd48657b763eac5f16
janis-jenny <pcardenasvm@gmail.com>
2020-10-28 14:59:35 -0500

    refactor check_winner test

--- spec/TicTacToe_spec.rb
+++ spec/TicTacToe_spec.rb
@@ -3,13 +3,7 @@ require './lib/game_logic'
 
 describe Game do
     let(:game) {Game.new}
-    let(:arr) {%w[x x x ]}
-    let(:simbol){'o'}
-    # describe "#display_board" do
-    #     it "should display the initial board of the game" do 
-    #     expect(game.).to eq(@board)
-    #     end
-    # end
+    let(:simbol){'X'}
 
     describe '#check_move' do
         it 'should replace the move of the player with the simbol' do
@@ -23,8 +17,6 @@ describe Game do
       end
 
       it 'should return true if the combination of simbols are right' do
-       
-        simbol = 'X'
         game.check_move(0, simbol)
         game.check_move(1, simbol)
         game.check_move(2, simbol)
